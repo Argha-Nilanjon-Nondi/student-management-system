08ddae4b7a96b1340bb9d5d98bb2355e370c32e9
Argha Nilanjon Nondi <pcic095@gmail.com>
2021-07-28 22:37:21 +0600

    login.php is changed

--- api/login.php
+++ api/login.php
@@ -3,18 +3,16 @@ class Login{
    public function logmein(){
     $objDatabase=new Database();
     $objDatabase->getConnection();
-    
-    $randnum=strval(mt_rand());
-     $objDatabase->sql="SELECT token FROM users WHERE email='$this->email' AND password=SHA2('$this->password',256);
-     UPDATE users set token=SHA2('$randnum',256) WHERE email='$this->email' AND password=SHA2('$this->password',256) ";
-     $sqlRep=$objDatabase->runSql();
-     if(count($sqlRep)==0){
-         $data=array();
-         $data["code"]="3000";
-         $data["message"]="Crediantials are not correct";
-         echo json_encode($data);
-         return 0;
-     }
+
+    $objValidation=new Validation();
+
+     if($objValidation->validEmailAndPassword($this->email,$this->password)==false){
+            $data=array();
+            $data["code"]="3000";
+            $data["message"]="Crediantials are not correct";
+            echo json_encode($data);
+            return 0;
+        }
 
      $objDatabase->sql="SELECT token,usertype FROM users WHERE email='$this->email' AND password=SHA2('$this->password',256);";
      $sqlRep=$objDatabase->runSql();
